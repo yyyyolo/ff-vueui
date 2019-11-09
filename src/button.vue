085c5645dbf312a7d0174457a678afb894eb85c9
Chenfen Chen <13476143115@163.com>
2019-11-09 20:05:11 +0800

    将 svg 代码整理到 icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-      <svg v-if="icon" class="icon">
-        <use :xlink:href="`#i-${icon}`" />
-      </svg>
+      <g-icon v-if="icon" :name="icon"></g-icon>
       <div class="content">
         <slot></slot>
       </div>
@@ -25,23 +23,6 @@ props: {
 };
 </script>
 <style lang="scss">
-/* .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
-    }
-  } */
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
