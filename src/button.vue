095fc26b356d210e1de1633409f39cfe67702dda
Chenfen Chen <13476143115@163.com>
2019-11-09 19:15:18 +0800

    填写 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,22 @@
 <template>
-<div>
-  <button class="g-button">按钮</button>
-</div>
-
+  <div>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href="`#i-${icon}`" />
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
+  </div>
 </template>
 <script>
-  export default {}
+export default {
+  props: ["icon", "iconPosition"]
+};
 </script>
 <style lang="scss">
-  .g-button {
+/* .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
@@ -24,5 +32,43 @@
     &:focus {
       outline: none;
     }
+  } */
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color-hover);
   }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+  }
+}
 </style> 
